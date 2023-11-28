ceba2d788d2d258890e3adf41ac4cbb17101d71b
NicoStellwag <nicolas.stellwag@tum.de>
2023-11-28 14:10:12 +0100

    add helpers to split images to const size crops and reconstruct

--- ncut/feature_extraction_2d/f2dutil.py
+++ ncut/feature_extraction_2d/f2dutil.py
@@ -1,8 +1,8 @@
 import cv2
 import torchvision.transforms.functional as F
 from torch.utils.data import Dataset
-import torch.nn.functional as nnF
 import torch
+import numpy as np
 from pathlib import Path
 import os
 
@@ -54,52 +54,53 @@ def color_images_from_sens_file(path, image_size=None, frame_skip=1):
     yield from color_images_from_sensor_data(data, image_size, frame_skip)
 
 
-def split_to_patches(image_tensor, patch_size):
+def split_to_patches(img, patch_size):
     """
     Split torch image tensor of shape (C, H, W)
-    to a batch of square patches (patches, C, patch_size, patch_size).
+    to a list of square patches [(C, patch_size, patch_size), ...].
     Pad if necessary.
-    Returns (patches, pad_height, pad_width) - padding dims needed for reconstruction.
+    Returns: patches, pad_height, pad_width, num_rows, num_cols
     """
-    C, H, W = image_tensor.size()
-    pad_height = (patch_size - H % patch_size) % patch_size
-    pad_width = (patch_size - W % patch_size) % patch_size
+    img = img.numpy()
+    pad_h = (patch_size - (img.shape[1] % patch_size)) % patch_size
+    pad_w = (patch_size - (img.shape[2] % patch_size)) % patch_size
+    padded_img = np.pad(img, ((0, 0), (0, pad_h), (0, pad_w)))
 
-    # Padding the image if necessary
-    image_tensor = F.pad(image_tensor, (0, pad_width, 0, pad_height), padding_mode='constant', fill=0)
+    num_rows = padded_img.shape[1] // patch_size
+    num_cols = padded_img.shape[2] // patch_size
 
-    # Using unfold to split the image into patches
-    patches = image_tensor.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size)
-    patches = patches.contiguous().view(-1, C, patch_size, patch_size)
-    return patches, pad_height, pad_width
+    patches = []
+    for row in range(num_rows):
+        for col in range(num_cols):
+            h_from = row * patch_size
+            h_to = (row + 1) * patch_size
+            w_from = col * patch_size
+            w_to = (col + 1) * patch_size
+            patches.append(padded_img[:, h_from:h_to, w_from:w_to])
+
+    print(patches[0].shape)
+    tensor_patches = [torch.from_numpy(i) for i in patches]
+    print(tensor_patches[0].shape)
+    return tensor_patches, pad_h, pad_w, num_rows, num_cols
 
 
-
-# todo fix this
-def reconstruct_from_patches(patches, original_size, pad_height, pad_width):
+def reconstruct_from_patches(
+    patches, original_h, original_w, pad_h, pad_w, num_rows, num_cols
+):
     """
     Reconstruct torch image tensor with original dimensions
     from a batch of patches and remove padding.
     """
-    _, H, W = original_size
-    n_patches, channels, patch_size, _ = patches.size()
-
-    # Calculate the total number of patch positions
-    num_patches_h = (H + pad_height) // patch_size
-    num_patches_w = (W + pad_width) // patch_size
-    total_patches = num_patches_h * num_patches_w
-
-    # Ensure that the number of patches matches the expected number
-    assert n_patches == total_patches, "Number of patches does not match expected total."
-
-    # Reshape patches for the fold operation
-    patches = patches.permute(0, 2, 3, 1).reshape(1, channels * patch_size * patch_size, -1)
-
-    # Reconstruct the image using fold
-    output = nnF.fold(patches, output_size=(H + pad_height, W + pad_width), kernel_size=patch_size, stride=patch_size)
-
-    # Remove the padding
-    if pad_height > 0 or pad_width > 0:
-        output = output[:, :, :H, :W]
-
-    return output
+    feature_channels, patch_size, _ = patches[0].shape
+    reconstructed = torch.empty(
+        (feature_channels, original_h + pad_h, original_w + pad_w)
+    )
+    for row in range(num_rows):
+        for col in range(num_cols):
+            h_from = row * patch_size
+            h_to = (row + 1) * patch_size
+            w_from = col * patch_size
+            w_to = (col + 1) * patch_size
+            reconstructed[:, h_from:h_to, w_from:w_to] = patches[row * num_cols + col]
+    original_reconstructed = reconstructed[:, :original_h, :original_w]
+    return original_reconstructed

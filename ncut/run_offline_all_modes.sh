05878ade12e662ee3da1ba770b6373a4362fd85c
NicoStellwag <nicolas.stellwag@tum.de>
2024-01-16 17:34:56 +0100

    prepare next training run - dropout before label mlp - drop loss mean over active masks - mask loss mean instead of sum aggr => lr * 5 - for training, do not filter out any classes (we can't know them wo/ gt)

--- ncut/run_offline_all_modes.sh
+++ ncut/run_offline_all_modes.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-
-python ./ncut/run_offline.py ncut.data.dataset.mode=train
-python ./ncut/run_offline.py ncut.data.dataset.mode=val
-python ./ncut/run_offline.py ncut.data.dataset.mode=test
+python ./ncut/run_offline.py ncut.use_ds_gt_segmentation="false" ncut.data.dataset.mode=train
+python ./ncut/run_offline.py ncut.use_ds_gt_segmentation="true" ncut.data.dataset.mode=val
